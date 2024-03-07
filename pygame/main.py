47a25e258e6dfb7c25264a992ba9f8f380b84ced
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-03-07 18:21:15 -0500

    added player image, set size to properly render on the screen, as well as propoperly set coordinate to be in the middle of the x-axis and at the bottom of the y-axis& C:/Users/malli/AppData/Local/Programs/Python/Python311/python.exe c:/Dev/python-intermediate/pygame/main.py

--- pygame/main.py
+++ pygame/main.py
@@ -4,7 +4,7 @@ import pygame
 # pygame setup and initiliaze pygame
 pygame.init()
 # Create the screen and set screen dimension
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((800, 600))
 
 
 # set title and icon
@@ -13,6 +13,25 @@ pygame.display.set_caption('Space Invation.')
 icon_image = pygame.image.load('rocket-ship.png')
 # store the image in the set icon method
 pygame.display.set_icon(icon_image)
+
+# CREATE PLAYER FOR THE GAME
+# load image that will rapresent the player
+img_player = pygame.image.load('player_img.png')
+
+# since the image of the icon is extremely large we are going to chage its dimension
+new_size = (75,75)
+img_player = pygame.transform.scale(img_player, new_size)
+
+# establish the x, y coordinate that will move the plyaer around
+player_x = 362.5
+player_y = 525
+
+# create function that will display player
+def player():
+    screen.blit(img_player, (player_x, player_y))
+
+
+
 
 # variable that will dictate the while loop condition
 is_running = True
@@ -30,6 +49,8 @@ while is_running:
     background_color = "violet"
     background_rgb = (205,144,228)
     screen.fill(background_rgb)
+
+    player()
 
      # flip() the display to put your work on screen
     pygame.display.update()
