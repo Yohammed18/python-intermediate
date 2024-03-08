eefac25ea3556d897fcd63b31ddf5b3b8688bcc4
Mohammed Allibalogun <mallibalogun18@gmail.com>
2024-03-07 19:31:46 -0500

    set controlling movements to properly set the player rocket coordinates on the x,y axis based on the up,down,left, right key board when it's pressed.

--- pygame/main.py
+++ pygame/main.py
@@ -22,13 +22,16 @@ img_player = pygame.image.load('player_img.png')
 new_size = (75,75)
 img_player = pygame.transform.scale(img_player, new_size)
 
+# when setting coordinate always remmber The (0, 0) coordinates represent the top-left corner of the surface.
 # establish the x, y coordinate that will move the plyaer around
 player_x = 362.5
 player_y = 525
+player_x_change = 0
+player_y_change = 0
 
 # create function that will display player
-def player():
-    screen.blit(img_player, (player_x, player_y))
+def player(x, y):
+    screen.blit(img_player, (x , y))
 
 
 
@@ -42,15 +45,50 @@ while is_running:
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            is_running = False 
+            is_running = False
+
+        # Controlling the direction of the rocket with our key board
+        # remeber everything is an event and we will program as such
+        # First check if any keys were pressed down
+        if event.type == pygame.KEYDOWN:
+            # handles the x-axis movements 
+            if event.key == pygame.K_LEFT:
+                player_x_change = -0.3
+            elif event.key == pygame.K_RIGHT:
+                player_x_change = 0.3
+            # handle the y-axis movement
+            elif event.key == pygame.K_UP:
+                player_y_change = -0.3
+            elif event.key == pygame.K_DOWN:
+                player_y_change = 0.3
+
+    
+        
+        # this is needed to handle the position of the space ship the the key is no longer press
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player_x_change = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                player_y_change = 0
+
+
 
     # all the update within the game must be done insided of the while loop
     # You will use rgb colors numbers to set the background of the screen, however you can also just name the color and pass it as a string
     background_color = "violet"
     background_rgb = (205,144,228)
     screen.fill(background_rgb)
+    # value that will move the player up or down depending on which axis you're manipulating with
+    # player_x += 0.1
+    # player_y -= 0.1
 
-    player()
+
+    # set the coordinate of the player on the x,y axis when the user press on the up, down, left or right key
+    player_x += player_x_change
+    player_y += player_y_change
+
+
+    player(player_x, player_y)
 
      # flip() the display to put your work on screen
     pygame.display.update()
